9c7578c914f501afc861792ae45d07dec6e2b282
ref1821 <ref1821@gmail.com>
2022-09-18 19:20:38 -0500

    drivers update

---
+++ src/pages/Data.js
@@ -0,0 +1,545 @@
+import React from 'react'
+export const Ver = 325
+export const Per = 210
+export const Lec = 219
+export const Sai = 187
+export const Ham = 168
+export const Rus = 203
+export const Nor = 88
+export const Ric = 19
+export const Alo = 59
+export const Oco = 66
+export const Mag = 22
+export const Msc = 12
+export const Bot = 46
+export const Zho = 6
+export const Gas = 22
+export const Tsu = 11
+export const Vet = 20
+export const Str = 5
+export const Alb = 4
+export const Lat = 0
+export const Dev = 2
+export const Hul = 0
+export const Data = [
+    Ver, Per, Lec, Sai, Ham, Rus, Nor, Ric, Alo, Oco, Mag, Msc, Bot, Zho, Gas, Tsu, Vet, Str, Alb, Lat, Dev, Hul
+]
+export const Verdata = {
+    labels: ['WINS', 'PODIUM','POINTS FINISH', 'DNF'],
+    datasets: [
+      {
+        label: 'RACES',
+        data: [11, 2, 1, 2],
+        backgroundColor: [
+          '#ffd32a',
+          '#d2dae2',
+          '#05c46b',
+          '#ff3f34',
+        ],
+        borderColor: [
+          '#ffd32a',
+          '#d2dae2',
+          '#05c46b',
+          '#ff3f34',
+        ],
+        borderWidth: 1,
+        },
+    ],
+};
+export const Lecdata = {
+    labels: ['WINS', 'PODIUM','POINTS FINISH', 'DNF'],
+    datasets: [
+      {
+        label: 'RACES',
+        data: [3, 4, 6, 3],
+        backgroundColor: [
+          '#ffd32a',
+          '#d2dae2',
+          '#05c46b',
+          '#ff3f34',
+        ],
+        borderColor: [
+          '#ffd32a',
+          '#d2dae2',
+          '#05c46b',
+          '#ff3f34',
+        ],
+        borderWidth: 1,
+        },
+    ],
+};
+export const Perdata = {
+    labels: ['WINS', 'PODIUM','POINTS FINISH', 'DNF'],
+    datasets: [
+      {
+        label: 'RACES',
+        data: [1, 6, 6, 3],
+        backgroundColor: [
+          '#ffd32a',
+          '#d2dae2',
+          '#05c46b',
+          '#ff3f34',
+        ],
+        borderColor: [
+          '#ffd32a',
+          '#d2dae2',
+           '#05c46b',
+          '#ff3f34',
+        ],
+        borderWidth: 1,
+        },
+    ],
+};
+export const Rusdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'DNF'],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 8, 7, 1],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+         '#05c46b',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Saidata = {
+    labels: ['WINS', 'PODIUM','POINTS FINISH', 'DNF'],
+    datasets: [
+      {
+        label: 'RACES',
+        data: [1, 6, 5, 4],
+        backgroundColor: [
+          '#ffd32a',
+          '#d2dae2',
+          '#05c46b',
+          '#ff3f34',
+        ],
+        borderColor: [
+          '#ffd32a',
+          '#d2dae2',
+          '#05c46b',
+          '#ff3f34',
+        ],
+        borderWidth: 1,
+        },
+    ],
+};
+export const Hamdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH','No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 6, 8, 1, 1],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Nordata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 1, 11, 2, 2],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Ocodata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 12, 2, 2],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Botdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 7, 4, 5],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Alodata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 11, 2, 3],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Magdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 5, 7, 4],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Ricdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 5, 9, 2],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Gasdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 5, 8, 3],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Vetdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 6, 5, 5],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Mscdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 2, 11, 3],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Tsudata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 3, 9, 4],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Zhodata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 3, 9, 4],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Strdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 5, 9, 2],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Albdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 3, 9, 3],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Latdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 0, 13, 3],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Devdata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 1, 0, 0],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+export const Huldata = {
+  labels: ['WINS', 'PODIUM','POINTS FINISH', 'No points','DNF' ],
+  datasets: [
+    {
+      label: 'RACES',
+      data: [0, 0, 0, 2, 0],
+      backgroundColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderColor: [
+        '#ffd32a',
+        '#d2dae2',
+        '#05c46b',
+        '#ff6348',
+        '#ff3f34',
+      ],
+      borderWidth: 1,
+      },
+  ],
+};
+
